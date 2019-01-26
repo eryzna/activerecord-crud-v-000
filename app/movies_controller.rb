eddab8032303dc8960d142dbd4b13e7e9fc73e59
Elana Ryznar <eryzna@gmail.com>
2019-01-26 03:12:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -60,7 +60,7 @@ end
 def can_be_found_updated_and_saved
   movie = Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title:"Awesome Flick")
-  movie.update(title: "Pretty Sweet Flick")
+  movie.update(title: "Even Awesomer Flick")
   movie.save
 end
 
